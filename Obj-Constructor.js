a24c935810c38535a9df4032055d9e26a8ae88ef
Patrick Oser <patrick.oser148@gmail.com>
2023-05-13 12:07:00 -0400

    Wrapped JS code in addEventListner so that DOM content loads first.

--- Obj-Constructor.js
+++ Obj-Constructor.js
@@ -1,41 +1,44 @@
-let myLibrary = [];
+document.addEventListener("DOMContentLoaded", () => {
+  let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.getInfo = function () {
-    return `Title: ${this.title}\nAuthor: ${this.author}\nPages: ${this.pages}\nStatus: ${this.read}.`;
-  };
-}
-
-const myForm = document.getElementById("myForm");
-
-function addBookToLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
-  return myLibrary.push(book);
-}
-
-function showLibrary() {
-  const mainContent = document.querySelector(".main-content");
-  mainContent.textContent = "";
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement("div");
-    card.textContent = myLibrary[i].getInfo();
-    mainContent.appendChild(card);
+  function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.getInfo = function () {
+      return `"Title: ${this.title}\nAuthor: ${this.author}\nPages: ${this.pages}\nStatus: ${this.read}."`;
+    };
   }
-}
 
-myForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+  const myForm = document.getElementById("myForm");
 
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const status = document.getElementById("status").value;
+  function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    return myLibrary.push(book);
+  }
 
-  addBookToLibrary(title, author, pages, status);
-  showLibrary();
+  function showLibrary() {
+    const mainContent = document.querySelector(".main-content");
+    mainContent.textContent = "";
+
+    for (let i = 0; i < myLibrary.length; i++) {
+      const card = document.createElement("div");
+      card.textContent = myLibrary[i].getInfo();
+      mainContent.appendChild(card);
+    }
+  }
+
+  myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const status = document.getElementById("status").value;
+
+    addBookToLibrary(title, author, pages, status);
+    showLibrary();
+    myForm.inputMode.value = "";
+  });
 });
